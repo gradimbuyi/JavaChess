8458935a55988fa16ff7729d742cdc966e2db7b0
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-12-20 14:05:53 -0500

    Modify pieces package
    Added functionality (the ability to castle) to King class. Modified legal move implementation of each piece. Further refactoring is in order.

---
+++ src/pieces/Knight.java
@@ -0,0 +1,73 @@
+package pieces;
+
+import graphic.Board;
+import graphic.Tile;
+import java.util.ArrayList;
+
+/**
+ * @author Gradi Tshielekeja Mbuyi
+ * @version 1.0
+ */
+public class Knight extends Piece {
+    public Knight(String type, boolean color, int locationX, int locationY) {
+        super(type, color, locationX, locationY);
+    }
+
+
+    @Override
+    public ArrayList<Tile> legalMoves() {
+        Tile[][] board = Board.getBoard();
+        moves = new ArrayList<>();
+
+        if(locationX - 2 >= 0) {
+            if((locationY - 1 >= 0) && (board[locationX - 2][locationY - 1].getPiece() == null ||
+                board[locationX - 2][locationY - 1].getPiece().getColor() != color)) {
+                moves.add(board[locationX - 2][locationY - 1]);
+            }
+
+            if((locationY + 1 <= 7) && (board[locationX - 2][locationY + 1].getPiece() == null ||
+                board[locationX - 2][locationY + 1].getPiece().getColor() != color)) {
+                moves.add(board[locationX - 2][locationY + 1]);
+            }
+        }
+
+        if(locationX + 2 <= 7) {
+            if((locationY - 1 >= 0) && (board[locationX + 2][locationY - 1].getPiece() == null ||
+                board[locationX + 2][locationY - 1].getPiece().getColor() != color)) {
+                moves.add(board[locationX + 2][locationY - 1]);
+            }
+
+            if((locationY + 1 <= 7) && (board[locationX + 2][locationY + 1].getPiece() == null ||
+                board[locationX + 2][locationY + 1].getPiece().getColor() != color)) {
+                moves.add(board[locationX + 2][locationY + 1]);
+            }
+        }
+
+        if(locationY - 2 >= 0) {
+            if((locationX - 1 >= 0) && (board[locationX - 1][locationY - 2].getPiece() == null ||
+                board[locationX - 1][locationY - 2].getPiece().getColor() != color)) {
+                moves.add(board[locationX - 1][locationY - 2]);
+            }
+
+            if((locationX + 1 <= 7) && (board[locationX + 1][locationY - 2].getPiece() == null ||
+                board[locationX + 1][locationY - 2].getPiece().getColor() != color)) {
+                moves.add(board[locationX + 1][locationY - 2]);
+            }
+        }
+
+        if(locationY + 2 <= 7) {
+            if((locationX - 1 >= 0) && (board[locationX - 1][locationY + 2].getPiece() == null ||
+                board[locationX - 1][locationY + 2].getPiece().getColor() != color)) {
+                moves.add(board[locationX - 1][locationY + 2]);
+            }
+
+            if((locationX + 1 <= 7) && (board[locationX + 1][locationY + 2].getPiece() == null ||
+                board[locationX + 1][locationY + 2].getPiece().getColor() != color)) {
+                moves.add(board[locationX + 1][locationY + 2]);
+            }
+        }
+
+        return moves;
+    }
+
+}
