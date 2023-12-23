75dc7d42b29c8f1109f6691fa286bc1613323a23
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-12-22 21:38:16 -0500

    Major update to Piece.java

--- src/com/github/grxdiii/chess/pieces/Piece.java
+++ src/com/github/grxdiii/chess/pieces/Piece.java
@@ -1,9 +1,11 @@
 package com.github.grxdiii.chess.pieces;
 
+// Personal libraries
 import com.github.grxdiii.chess.graphic.Board;
 import com.github.grxdiii.chess.graphic.Tile;
 import com.github.grxdiii.chess.main.Game;
 
+// Java Libraries
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Image;
@@ -14,96 +16,130 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public abstract class Piece extends JLabel {
+    // PROPERTIES OF CHESS PIECES
     protected String type;
     protected final Boolean color;
     protected Integer locationX;
     protected Integer locationY;
     protected Integer numMoves;
     protected ArrayList<Tile> moves;
+
+    // Static variable to access the Chess Board
     protected static Tile[][] board = Board.getBoard();
 
     /**
+     * The Piece constructors takes in four parameters which allows it to take on different properties.
+     * Based on the parameters, the piece is generated with a specific name, color, and location
+     * within the chess board.
      *
-     * @param type
-     * @param color
-     * @param locationX
-     * @param locationY
+     * @param type the name (type) of the chess piece
+     * @param color the color of the chess piece
+     * @param locationX the x location where the chess piece is generated
+     * @param locationY the y location where the chess piece is generated
      */
     public Piece(String type, boolean color, int locationX, int locationY) {
+        // Store the given parameters in their appropriate properties
         this.type = type;
         this.color = color;
         this.locationX = locationX;
         this.locationY = locationY;
         this.numMoves = 0;
-        if(this.color) setImage("./images/white_" + this.type + ".png");
-        else setImage("./images/black_" + this.type + ".png");
+
+        // Builds the name of piece image file based off it type and color
+        String imageFileName = color ? "./images/white_" + this.type + ".png":
+                                       "./images/black_" + this.type + ".png";
+
+        // Creates an image icon for the piece - which is displayed on the chess board
+        Image image = new ImageIcon(imageFileName).getImage();
+              image = image.getScaledInstance(105, 105, Image.SCALE_SMOOTH);
+
+        // Displays the chess piece on a given tile
+        this.setIcon(new ImageIcon(image));
     }
 
     /**
+     * This method checks to see if a tile, given a locationX and locationY is occupied. In
+     * the case where it is, the method returns true if the tile is occupied by an enemy piece.
+     * The method also returns true if the tile is unoccupied.
      *
-     * @return
+     * @param locationX the x location of a potential tile
+     * @param locationY the y location of a potential tile
+     * @return Returns true if the tiles is unoccupied or is occupied by enemy piece, otherwise
+     *         returns false
      */
-    public abstract ArrayList<Tile> legalMoves();
+    protected Boolean isTileAvailable(int locationX, int locationY) {
+        // returns true if all conditions are met
+        if(locationX >= 0 && locationX <= 7 && locationY >= 0 && locationY <= 7) {
+            Piece piece = board[locationX][locationY].getPiece();
+            return piece == null || piece.getColor() != color;
+        }
 
-    /**
-     *
-     * @param moves
-     * @param destinationTile
-     * @return
-     */
-    public Boolean checkIfValid(ArrayList<Tile> moves, Tile destinationTile) {
-        if(moves.size() > 0) for(Tile move : moves) if(move == destinationTile) return true;
+        // otherwise returns false
         return false;
     }
 
     /**
+     * This abstract method is defined by every class that extends the Piece class. Every chess
+     * piece has a way in which it moves and the legalMoves() method is in charge of
+     * making sure that the moves made by the players are legal.
      *
-     * @param fileName
+     * @return Returns array list of legal moves of a given chess piece
      */
-    public void setImage(String fileName) {
-        this.setIcon(new ImageIcon(new ImageIcon(fileName).getImage().
-                getScaledInstance(105, 105, Image.SCALE_SMOOTH)));
-    }
+    public abstract ArrayList<Tile> legalMoves();
 
     /**
+     * This method calculates the legal moves of a given piece, and move the piece if the given
+     * destination tile is one of the
      *
-     * @param sourceTile
-     * @param destinationTile
-     * @return
+     * @param sourceTile the source where the piece resides
+     * @param destinationTile the destination where the piece would like to move
+     * @return Returns true if move has lead to a capture, otherwise returns false
      */
     public Boolean movePiece(Tile sourceTile, Tile destinationTile) {
-        boolean hasCaptured;
+        // Checks to see if all the conditions for castling are met. In the case where they
+        // are, the method performCastling() is called and further conditions are checked
+        if(this instanceof King && numMoves == 0) {
+            int source = sourceTile.getLocationX();
+            int destination = destinationTile.getLocationX();
 
-        if(this instanceof King && this.numMoves == 0 && sourceTile.getLocationX().intValue() == destinationTile.getLocationX().intValue() &&
-          (destinationTile.getLocationY() <= 2 || destinationTile.getLocationY() >= 6)) {
+            if(source == destination) {
+                int locationY = destinationTile.getLocationY();
 
-               if(destinationTile.getLocationY() <= 2 && ((King) this).performCastling(false)) return false;
-          else if(destinationTile.getLocationY() >= 6 && ((King) this).performCastling(true))  return false;
-
+                // If castling was successfully performed, the method returns false, otherwise,
+                // we continue and calculate the king's legal moves
+                if(locationY <= 2 || locationY >= 6) {
+                    boolean castle = ((King) this).performCastling(locationY);
+                    if(castle) return false;
+                }
+            }
         }
 
+        // This part of the code goes through the array list of the legal moves, comparing
+        // each legal move with the destination tile. In the case where a move is equal
+        // to the destination tile, this method allows the user to make the move.
+        for(Tile move : legalMoves()) {
+            if (move == destinationTile) {
+                sourceTile.removePiece();
+                Game.shouldWhiteMove = !Game.shouldWhiteMove;
+                numMoves++;
 
-        if(!checkIfValid(legalMoves(), destinationTile)) {
-            sourceTile.addPiece(this);
-            return false;
+                // adds piece to destination tile
+                return destinationTile.addPiece(this);
+            }
         }
 
-        hasCaptured = destinationTile.addPiece(this);
-
-        if(sourceTile != destinationTile) {
-            sourceTile.removePiece();
-            Game.shouldWhiteMove = !Game.shouldWhiteMove;
-            numMoves++;
-        }
-
-        return hasCaptured;
+        // No legal moves were found
+        sourceTile.addPiece(this);
+        return false;
     }
 
+    // HELPER METHOD FOR PIECE - ALLOWS US TO CHANE THE PIECE LOCATION X and Y
     public void changeLocation(int locationX, int locationY) {
         this.locationX = locationX;
         this.locationY = locationY;
     }
 
+    // GETTER METHODS
     public Boolean getColor() {
         return color;
     }
