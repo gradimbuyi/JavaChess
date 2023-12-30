af53e70f65baaac1c093b5890f296902d78d6bd4
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-12-29 20:58:44 -0500

    Update Tile.java

--- src/com/github/grxdiii/chess/graphic/Tile.java
+++ src/com/github/grxdiii/chess/graphic/Tile.java
@@ -1,7 +1,11 @@
 package com.github.grxdiii.chess.graphic;
 
+import com.github.grxdiii.chess.main.Game;
 import com.github.grxdiii.chess.pieces.Piece;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
 
 
 /**
@@ -12,43 +16,91 @@ import javax.swing.JPanel;
  * @version 1.0
  */
 public class Tile extends JPanel {
-    private final Integer locationX;
-    private final Integer locationY;
+    private static final Integer[]   TILE_RANK = {8, 7, 6, 5, 4, 3, 2, 1};
+    private static final Character[] TILE_FILE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    private static final Color LIGHT = Color.decode("#b5d3ff");
+    private static final Color DARK  = Color.decode("#4c8aed");
+    private final Integer location_x;
+    private final Integer location_y;
+    private final Color color;
     private Piece piece;
-    public Boolean isOccupied;
+    private Boolean hasPiece;
 
     /**
      * The TIle constructor takes in two parameters: locationX and locationY.
      *
-     * @param locationX
-     * @param locationY
+     * @param location_x
+     * @param location_y
      */
-    public Tile(int locationX, int locationY) {
-        this.locationX = locationX;
-        this.locationY = locationY;
+    public Tile(int location_x, int location_y, Color color) {
+        this.location_x = location_x;
+        this.location_y = location_y;
+        this.color = color;
         this.piece = null;
-        this.isOccupied = false;
+        this.hasPiece = false;
+        this.setBackground(color);
     }
 
+    /**
+     *
+     */
     public void removePiece() {
         if(this.piece == null) return;
-
-        isOccupied = false;
+        hasPiece = false;
         remove(piece);
         piece = null;
         repaint();
     }
 
+    /**
+     *
+     * @param piece
+     * @return
+     */
     public boolean addPiece(Piece piece) {
-        boolean hasCaptured = isOccupied;
+        boolean hasCaptured = hasPiece;
         removePiece();
         this.piece = piece;
-        this.piece.changeLocation(locationX, locationY);
-        this.isOccupied = true;
+        this.hasPiece = true;
+        this.piece.changeLocation(location_x, location_y);
         this.add(piece);
-        this.validate();
-        this.repaint();
+        repaint();
         return hasCaptured;
+    }
+
+    /**
+     *
+     * @param graphics the <code>Graphics</code> object to protect
+     */
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        Piece selected_piece = Game.getSelectedPiece();
+        Color graphics_light = Board.getLightTileColor();
+        Color graphics_dark  = Board.getDarkTileColor();
+
+        super.paintComponent(graphics);
+
+        if(location_x == 7 || location_y == 7) {
+            Color graphics_color = color.equals(graphics_light) ? graphics_dark : graphics_light;
+            Font  graphics_font  = new Font("Arial", Font.PLAIN, 15);
+
+            // Set graphics font and color
+            graphics.setFont(graphics_font);
+            graphics.setColor(graphics_color);
+
+            // Draw rank and file
+            if(location_y == 7) {
+                String rank = TILE_RANK[location_x].toString();
+                graphics.drawString(rank, 102, 20);
+            }
+
+            if(location_x == 7) {
+                String file = TILE_FILE[location_y].toString();
+                graphics.drawString(file, 5, 110);
+            }
+        }
+
+        // Board.updateBoard(selected_piece, graphics);
     }
 
     public Piece getPiece() {
@@ -56,15 +108,27 @@ public class Tile extends JPanel {
     }
 
     public Integer getLocationX() {
-        return locationX;
+        return location_x;
     }
 
     public Integer getLocationY() {
-        return locationY;
+        return location_y;
     }
 
-    public Boolean checkPieceColor() {
-        if(isOccupied) return !piece.getColor();
-        return null;
+    public Boolean isOccupied() {
+        return hasPiece;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Color getNewColor() {
+        Color light = Board.getLightTileColor();
+        return color == light ? LIGHT : DARK;
+    }
+
+    public void resetColor() {
+        setBackground(color);
     }
 }
